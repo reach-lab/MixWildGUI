016d15070de3d8db4dccd0556c542ba75f81db2b
Eldin Dzubur <dzubur@usc.edu>
2016-11-22 10:48:21 -0800

    First commit

--- DefinitionHelper.java
+++ DefinitionHelper.java
@@ -6,6 +6,13 @@ public class DefinitionHelper {
     public static final int MIXREGMLS_MIXREG_KEY = 3;
     public static final int MIXREGMLS_MIXOR_KEY = 4;
 
+    /**
+     *
+     * @param randomLocationEffects, (int) the number of random location effects selected by user
+     * @param stageTwoBinary, (boolean) true if user selects dichotomous or ordinal outcome for stage two
+     * @return sequence Decision, (int) key determining stage 1+stage 2 model UI views
+     *
+     */
     public static int sequenceDecision(int randomLocationEffects, boolean stageTwoBinary) {
         if(stageTwoBinary){
             if(randomLocationEffects<2){return MIXREGLS_MIXOR_KEY;}
