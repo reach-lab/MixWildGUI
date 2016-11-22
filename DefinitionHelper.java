2c0a8cd594e7b541f40b7a0af76ca3086471d6a1
Eldin Dzubur <dzubur@usc.edu>
2016-11-22 10:35:18 -0800

    First commit

---
+++ DefinitionHelper.java
@@ -0,0 +1,19 @@
+
+
+public class DefinitionHelper {
+    public static final int MIXREGLS_MIXREG_KEY = 1;
+    public static final int MIXREGLS_MIXOR_KEY = 2;
+    public static final int MIXREGMLS_MIXREG_KEY = 3;
+    public static final int MIXREGMLS_MIXOR_KEY = 4;
+
+    public static int sequenceDecision(int randomLocationEffects, boolean stageTwoBinary) {
+        if(stageTwoBinary){
+            if(randomLocationEffects<2){return MIXREGLS_MIXOR_KEY;}
+            else{return MIXREGLS_MIXREG_KEY;}
+        }
+        else{
+            if(randomLocationEffects<2){return MIXREGMLS_MIXOR_KEY;}
+            else{return MIXREGMLS_MIXREG_KEY;}
+        }
+    }
+}
