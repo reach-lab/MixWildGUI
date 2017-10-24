5bf2c58b63a7672e2d9ba075ac1ee4fbc636c7dd
eldin <eldin@10.211.55.3>
2017-10-24 14:27:02 -0700

    New model builder class

---
+++ src/def_lib/ModelBuilder.java
@@ -0,0 +1,14 @@
+package def_lib;
+
+
+/**
+ * This class should be used to generate the model build after a file is run.
+ * @author eldin
+ */
+public class ModelBuilder {
+    
+    public ModelBuilder(DefinitionHelper defFile){
+        
+    }
+    
+}
