8bdf87aac33de41cc3e648e0381eac581a546e59
Aditya Ponnada <ponnada.a@husky.neu.edu>
2018-01-20 21:36:53 -0500

    Fix commit

--- src/def_lib/DefinitionHelper.java
+++ src/def_lib/DefinitionHelper.java
@@ -60,8 +60,8 @@ import org.apache.commons.io.FilenameUtils;
 
 /*****
  * Exposed Methods: // 
- * readDefinitionFile: takes File as parameter, assigns all
- * variables and then attempts to validate file buildStageOneDefinitionList:
+ * readDefinitionFile: takes File as parameter, assigns all variables and then attempts to validate file 
+ * buildStageOneDefinitionList:
  * returns ArrayList<String> that can be passed to a FileWriter class, validates first
  * @note: Both classes should pass Exceptions to the view and interrupt until user fixes error
  *
