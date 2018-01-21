c28fdcf9b660a75b2fbf89dbd8ede344aa94b6a6
Aditya Ponnada <ponnada.a@husky.neu.edu>
2018-01-20 21:07:09 -0500

    Fix commit

--- src/def_lib/DefinitionHelper.java
+++ src/def_lib/DefinitionHelper.java
@@ -56,9 +56,6 @@ import mixregui.NewModel;
 import mixregui.mixregGUI;
 import org.apache.commons.io.FilenameUtils;
 
-
-
-
 /*****
  * Exposed Methods: // readDefinitionFile: takes File as parameter, assigns all
  * variables and then attempts to validate file buildStageOneDefinitionList:
