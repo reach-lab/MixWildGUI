af105429602d95eeae811adec662a22e193c4925
Jixin <li.jix@husky.neu.edu>
2019-12-07 20:54:30 -0500

    create dir with human readable time

--- src/def_lib/ModelBuilder.java
+++ src/def_lib/ModelBuilder.java
@@ -25,9 +25,7 @@ MixWild, a program to model subject-level slope and variance on continuous or or
 	University of Chicago
 	DHedeker@health.bsd.uchicago.edu
 
-*/
-
-
+ */
 package def_lib;
 
 import java.awt.Font;
@@ -40,8 +38,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -50,27 +50,26 @@ import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 import org.apache.commons.io.FilenameUtils;
 
-
 /**
  * This class should be used to generate the model build after a file is run.
+ *
  * @author eldin
  */
 public class ModelBuilder {
-    
+
     private DefinitionHelper buildFile;
-    
+
     private static final String KEY_ALPHA = "\u03b1";
     private static final String KEY_BETA = "\u03b2";
     private static final String KEY_EPSILON = "\u03b5";
     private static final String KEY_I = "\u1d62";
     private static final String KEY_IJ = "\u1d62" + "\u2c7c";
     private static final String KEY_PLUS = " + ";
-    
-    
-    public ModelBuilder(DefinitionHelper defFile){
+
+    public ModelBuilder(DefinitionHelper defFile) {
         this.buildFile = defFile;
-        switch(buildFile.sequenceDecision()){
-            case DefinitionHelper.MIXREGLS_MIXREG_KEY:  
+        switch (buildFile.sequenceDecision()) {
+            case DefinitionHelper.MIXREGLS_MIXREG_KEY:
                 break;
             case DefinitionHelper.MIXREGLS_MIXOR_KEY:
                 break;
@@ -82,53 +81,51 @@ public class ModelBuilder {
                 break;
         }
     }
-    
-    public String meanEquation(){
+
+    public String meanEquation() {
         String prefix = "Y" + KEY_IJ + " = ";
         String constant = KEY_BETA + KEY_IJ + KEY_PLUS;
         String bsvar = "u" + KEY_I + KEY_PLUS;
         String wsvar = KEY_EPSILON + KEY_IJ;
-        if(buildFile.getModelFixedInt().matches("0")){
-            constant="";
+        if (buildFile.getModelFixedInt().matches("0")) {
+            constant = "";
         }
         String regressorsOne = "";
         String regressorsTwo = "";
         String[] meanArrayOne = buildFile.getLabelModelMeanRegressorsLevelOne();
         String[] meanArrayTwo = buildFile.getLabelModelMeanRegressorsLevelTwo();
         try {
-            for(int i = 0; i < meanArrayOne.length; i++){
-                regressorsOne += meanArrayOne[i].toUpperCase() + KEY_IJ + KEY_PLUS;    
+            for (int i = 0; i < meanArrayOne.length; i++) {
+                regressorsOne += meanArrayOne[i].toUpperCase() + KEY_IJ + KEY_PLUS;
             }
-        }
-        catch (NullPointerException npe){
-            
+        } catch (NullPointerException npe) {
+
         }
         try {
-            for(int i = 0; i < meanArrayTwo.length; i++){
-                regressorsOne += meanArrayTwo[i].toUpperCase() + KEY_I + KEY_PLUS;    
+            for (int i = 0; i < meanArrayTwo.length; i++) {
+                regressorsOne += meanArrayTwo[i].toUpperCase() + KEY_I + KEY_PLUS;
             }
+        } catch (NullPointerException npe) {
+
         }
-        catch(NullPointerException npe) {
-           
-        }
-           
+
         String regressors = regressorsOne + regressorsTwo;
         String[] decompMeanArray = buildFile.getLabelDecompMeanRegressors();
-        for(int i = 0; i < decompMeanArray.length; i++){
-            regressors += decompMeanArray[i].toUpperCase() + "_BS"+ KEY_BETA + KEY_I + KEY_PLUS + decompMeanArray[i].toUpperCase() + "_WS" + KEY_BETA + KEY_IJ + KEY_PLUS;    
+        for (int i = 0; i < decompMeanArray.length; i++) {
+            regressors += decompMeanArray[i].toUpperCase() + "_BS" + KEY_BETA + KEY_I + KEY_PLUS + decompMeanArray[i].toUpperCase() + "_WS" + KEY_BETA + KEY_IJ + KEY_PLUS;
         }
-        
+
         return prefix + constant + regressors + bsvar + wsvar;
     }
-    
+
     public static void saveWildDefinitionFile(String filePath, DefinitionHelper defLib) throws FileNotFoundException, IOException {
         FileOutputStream fos = new FileOutputStream(filePath);
-        ObjectOutputStream oos= new ObjectOutputStream(fos);
-        oos.writeObject(defLib);  
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(defLib);
         oos.close();
         fos.close();
     }
-    
+
     /*
     public static void saveWildFile(DefinitionHelper defLib) throws FileNotFoundException, IOException {
         FileInputStream in = new FileInputStream(defLib.getDataFilename());
@@ -159,32 +156,34 @@ public class ModelBuilder {
         out.close();
         in2.close();   
     }
-    */
-    
-    public static DefinitionHelper loadWildFile (String filePath) throws IOException, ClassNotFoundException {        
+     */
+    public static DefinitionHelper loadWildFile(String filePath) throws IOException, ClassNotFoundException {
         FileInputStream fis = new FileInputStream(filePath);
-        ObjectInputStream ino = new ObjectInputStream(fis);  
+        ObjectInputStream ino = new ObjectInputStream(fis);
         DefinitionHelper s;
-        s = (DefinitionHelper)ino.readObject(); 
+        s = (DefinitionHelper) ino.readObject();
         ino.close();
         fis.close();
         return s;
     }
-    
+
     public static String buildFolder(File csvFile) throws IOException {
         String absolutePath = csvFile.getAbsolutePath();
         String folderPath = FilenameUtils.getFullPath(absolutePath);
-        String dirName = Long.toString(Instant.now().getEpochSecond());
+//        String dirName = Long.toString(Instant.now().getEpochSecond());
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyyMMddHHmm");
+        Date date = new Date(System.currentTimeMillis());
+        String dirName = formatter.format(date);
         String newPath = folderPath + "MIXWILD" + dirName + "/";
         File dirGen = new File(newPath);
         System.out.println(newPath);
         boolean genTrue = dirGen.mkdirs();
-        if(!genTrue){
+        if (!genTrue) {
             throw new IOException("Cannot generate temporary work directory, please check folder permissions");
         }
-        return "MIXWILD"  + dirName + "/";
+        return "MIXWILD" + dirName + "/";
     }
-    
+
     public static void archiveFolder(DefinitionHelper defLib, String absoluteWorkingDirectory) throws IOException {
         System.out.println(defLib.getUtcDirPath());
         System.out.println(absoluteWorkingDirectory);
@@ -192,42 +191,40 @@ public class ModelBuilder {
         byte[] buffer = new byte[1024];
         FileOutputStream fos = null;
         ZipOutputStream zos = null;
-        String outputZip = absoluteWorkingDirectory +  defLib.getUtcDirPath().replace("/", "") + ".mwa";
+        String outputZip = absoluteWorkingDirectory + defLib.getUtcDirPath().replace("/", "") + ".mwa";
         List<String> fileList = new ArrayList<String>();
         saveWildDefinitionFile(folderPath + "model.mwd", defLib);
         createZipEntryList(new File(folderPath), fileList, folderPath);
-            
+
         fos = new FileOutputStream(outputZip);
         zos = new ZipOutputStream(fos);
 
         System.out.println("Input folderpath: " + folderPath);
         System.out.println("Output filename: " + outputZip);
 
+        for (String file : fileList) {
+            System.out.println("File in: " + file);
+            ZipEntry ze = new ZipEntry(file);
+            zos.putNextEntry(ze);
 
-        for(String file : fileList){
-                System.out.println("File in: " + file);
-                ZipEntry ze= new ZipEntry(file);
-                zos.putNextEntry(ze);
+            FileInputStream in
+                    = new FileInputStream(folderPath + "/" + file);
 
-                FileInputStream in =
-                       new FileInputStream(folderPath + "/" + file);
+            int len;
+            while ((len = in.read(buffer)) > 0) {
+                zos.write(buffer, 0, len);
+            }
 
-                int len;
-                while ((len = in.read(buffer)) > 0) {
-                        zos.write(buffer, 0, len);
-                }
-
-                in.close();
+            in.close();
         }
-        
 
         zos.closeEntry();
         zos.close();
-        
+
         System.out.println("Complete: Save");
-            
+
     }
-    
+
     public static DefinitionHelper accessFolderArchive(File mwaFile) throws IOException, ClassNotFoundException {
         String folderPath = FilenameUtils.getFullPath(mwaFile.getAbsolutePath());
         String dirName = Long.toString(Instant.now().getEpochSecond());
@@ -235,55 +232,55 @@ public class ModelBuilder {
         byte[] buffer = new byte[1024];
 
         File folder = new File(outputPath);
-    	if(!folder.exists()){
-    		folder.mkdir();
-    	}
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
         FileInputStream fis = new FileInputStream(mwaFile.getAbsolutePath());
-    	ZipInputStream zis = new ZipInputStream(fis);
-    	ZipEntry ze = zis.getNextEntry();
+        ZipInputStream zis = new ZipInputStream(fis);
+        ZipEntry ze = zis.getNextEntry();
 
-    	while(ze!=null){
-    	   String fileName = ze.getName();
-           File newFile = new File(outputPath + "/" + fileName);
-           System.out.println("Unzipped: "+ newFile.getAbsoluteFile());
-           
-           new File(newFile.getParent()).mkdirs();
-           FileOutputStream fos = new FileOutputStream(newFile);
-           int len;
-           while ((len = zis.read(buffer)) > 0) {
-               fos.write(buffer, 0, len);
-           }
-           fos.close();
-           ze = zis.getNextEntry();
-    	}
-        
+        while (ze != null) {
+            String fileName = ze.getName();
+            File newFile = new File(outputPath + "/" + fileName);
+            System.out.println("Unzipped: " + newFile.getAbsoluteFile());
+
+            new File(newFile.getParent()).mkdirs();
+            FileOutputStream fos = new FileOutputStream(newFile);
+            int len;
+            while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+            fos.close();
+            ze = zis.getNextEntry();
+        }
+
         zis.closeEntry();
-    	zis.close();
+        zis.close();
         fis.close();
 
         DefinitionHelper returnDefLib = loadWildFile(outputPath + "model.mwd");
         returnDefLib.setUtcDirPath(mwaFile.getAbsolutePath());
-     
-    	System.out.println("Complete: Open");
+
+        System.out.println("Complete: Open");
         return returnDefLib;
     }
-    
-    private static String createZipEntry(String file, String source){
-    	return file.substring(source.length(), file.length());
+
+    private static String createZipEntry(String file, String source) {
+        return file.substring(source.length(), file.length());
     }
-    
-    private static void createZipEntryList(File input, List fileList, String source){
-	if(input.isFile()){
+
+    private static void createZipEntryList(File input, List fileList, String source) {
+        if (input.isFile()) {
             fileList.add(createZipEntry(input.getAbsoluteFile().toString(), source));
-	}
+        }
 
-	if(input.isDirectory()){
-		String[] subInput = input.list();
-		for(String filename : subInput){
-			createZipEntryList(new File(input, filename), fileList, source);
-		}
-	}
+        if (input.isDirectory()) {
+            String[] subInput = input.list();
+            for (String filename : subInput) {
+                createZipEntryList(new File(input, filename), fileList, source);
+            }
+        }
 
     }
-    
-} 
+
+}
